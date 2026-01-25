509fc2ce076836de8df3f0c555f0a580ad58a961
srikesh2k4 <kotipallisrikesh@gmail.com>
2026-01-25 15:25:42 +0530

    feat(application): Contracts/Resositories were added

---
+++ backend/Bookstore.Application/Features/Books/Dtos/BookDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookstore.Application.Features.Books.Dtos
+{
+    internal class BookDto
+    {
+    }
+}
