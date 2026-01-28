0f7618d376c341600305a2eeb0bc31c270246bf7
Dinessh2815 <dinessh.venkat28@gmail.com>
2026-01-28 17:38:37 +0530

    fixed port errors

--- backend/Bookstore.Web/Program.cs
+++ backend/Bookstore.Web/Program.cs
@@ -135,7 +135,7 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 app.UseCors("BookstorePolicy");
 app.UseAuthentication();
 app.UseAuthorization();
